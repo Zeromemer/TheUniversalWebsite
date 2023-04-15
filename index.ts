a7b9d8f2624ba20d08e8dbc8a25613020867a30c
Zeromemer <zeromemer@users.noreply.github.com>
2023-04-16 02:05:35 +0300

    added /images/ endpoint

--- index.ts
+++ index.ts
@@ -12,8 +12,36 @@ const openai = new OpenAIApi(configuration);
 
 const app = express();
 
-const prompt = 'You are a webpage content generator and only return the HTML contents of a webpage from a url path. All webpage features (like scripts and stylesheets) shouldn\'t be within the page itself and not linked to.\n' +
-    'If you need to link an image use this AI image generator (for spaces in prompt use %20) https://image.pollinations.ai/prompt/{prompt}'
+const prompts = {
+    default: 'You are a webpage content generator. You are given a URL path and respond with the HTML code of the webpage.\n' +
+    'All images are in the /images/ folder.\n' +
+    'All webpage features (like scripts and stylesheets) shouldn\'t be within the page itself and not linked to.\n' +
+    'ONLY RESPOND WITH HTML!',
+    image: 'You are an AI image prompt generator, you are given a url for an image, and must create a very short description for an image to be generated.'
+};
+
+app.get('/images/*', async (req, res) => {
+    const path = req.url.substring(7);
+
+    const completion = await openai.createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        messages: [
+            { role: 'system', content: prompts.image },
+            { role: 'user', content: path }
+        ],
+        temperature: .95
+    });
+
+    const prompt = completion.data.choices[0].message?.content;
+
+    console.log(`image ${path} prompt: ${prompt}`);
+
+    if (!(prompt?.length)) {
+        throw new Error('No prompt');
+    }
+
+    res.redirect(`https://image.pollinations.ai/prompt/${encodeURIComponent(prompt)}`);
+});
 
 app.get('/favicon.ico', (req, res) => {
     res.status(404).send();
@@ -23,7 +51,7 @@ app.get('*', async (req, res) => {
     const completion = await openai.createChatCompletion({
         model: 'gpt-3.5-turbo',
         messages: [
-            { role: 'system', content: prompt },
+            { role: 'system', content: prompts.default },
             { role: 'user', content: req.url }
         ],
     });
